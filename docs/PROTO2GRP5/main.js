d423940f805affe1aef83a68da458cbdfb1c8723
lldennis <lldennis@ucsc.edu>
2023-11-09 14:15:29 -0800

    Removed gravity and change lose condition

--- docs/PROTO2GRP5/main.js
+++ docs/PROTO2GRP5/main.js
@@ -14,7 +14,6 @@ options = {
 let p, v;
 let floors;
 let jumpWay;
-let jumpPower;
 let floorAppDist;
 let scr;
 
@@ -23,16 +22,15 @@ function update() {
     p = vec(50, 50);
     v = vec();
     floors = [vec(50, 70)];
-    jumpWay = jumpPower = floorAppDist = 1;
+    jumpWay = floorAppDist = 1;
   }
   p.add(v);
-  v.y += input.isPressed ? 0.05 : 0.1;
-  score += scr = (p.y < 30 ? (30 - p.y) * 0.1 : 0) + difficulty * 0.1;
+  score += scr = (0.6) + difficulty * 0.1;
   if ((floorAppDist -= scr) < 0) {
     floorAppDist = rnd(99);
     floors.push(vec(rnd(99), -9));
   }
-  p.y += scr;
+  //p.y += scr;
   color("blue");
   floors = floors.filter((f) => {
     f.y += scr;
@@ -40,23 +38,15 @@ function update() {
     return f.y < 99;
   });
   color("transparent");
-  for (;;) {
-    if (!box(p, 7, 7).isColliding.rect.blue) {
-      break;
-    }
-    p.y--;
-    v.set();
-    jumpPower = 1;
-  }
   color("green");
   box(p, 7, 7);
   if (input.isJustPressed) {
     play("jump");
     v.x = jumpWay *= -1;
-    v.y = -3 * jumpPower;
-    jumpPower *= 0.7;
   }
-  if (p.y > 99) {
+  else if(p.x < 5 || p.x > 96.5)
+    v.x = 0;
+  if (box(p, 7, 7).isColliding.rect.blue) {
     play("explosion");
     end();
   }
